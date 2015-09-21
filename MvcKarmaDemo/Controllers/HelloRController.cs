59f1b9f7fbf01680b41a4c7276cb85949d41fff8
Mattias Åslund <mattias.aslund@mantex.se>
2015-09-21 21:26:29 +0700

    Added vanilla SignalR demo to MvcKarmaDemo. Test by pointing browsers to http://localhost:57429/HelloR.

---
+++ MvcKarmaDemo/Controllers/HelloRController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKarmaDemo.Controllers
+{
+    public class HelloRController : Controller
+    {
+        // GET: HelloR
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MvcKarmaDemo/Startup.cs
+++ MvcKarmaDemo/Startup.cs
@@ -8,7 +8,8 @@ namespace MvcKarmaDemo
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+			app.MapSignalR();
+			ConfigureAuth(app);
         }
     }
 }
