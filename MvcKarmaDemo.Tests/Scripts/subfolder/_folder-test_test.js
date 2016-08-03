0eb1b705c9540b8737dba2f3cf18ab4c210c4ff9
Mattias Åslund <mattias@trustfall.se>
2016-08-03 13:13:28 +0200

    Asserting Minimatch expressions work in paths.

---
+++ MvcKarmaDemo.Tests/Scripts/subfolder/_folder-test_test.js
@@ -0,0 +1,12 @@
+(function() {
+	"use strict";
+
+    describe("Folder Test", function () {
+
+        it("gets started from Web.config /**/ path", function () {
+            dump("Folder Test is running");
+            expect(true).to.equal(true);
+        });
+    });
+
+}());
